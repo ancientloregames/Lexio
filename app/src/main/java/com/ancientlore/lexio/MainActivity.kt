410df35758baa5aca08734085636d928b5b8679d
Nickolas Makhyboroda <ancientloregames@gmail.com>
2018-06-21 14:46:57 +0200

    added words list loading

--- app/src/main/java/com/ancientlore/lexio/MainActivity.kt
+++ app/src/main/java/com/ancientlore/lexio/MainActivity.kt
@@ -2,11 +2,30 @@ package com.ancientlore.lexio
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
+
+	private val dbExec: ExecutorService = Executors.newSingleThreadExecutor()
+
+	private lateinit var listAdapter: WordsListAdapter
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
+
+		val listView: RecyclerView = findViewById(R.id.listView)
+
+		listView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+
+		val db = WordsDatabase.getInstance(this)
+
+		dbExec.submit {
+			listAdapter = WordsListAdapter(db.wordDao().getAll().toMutableList())
+			listView.adapter = listAdapter
+		}
 	}
 }
---
+++ app/src/main/java/com/ancientlore/lexio/WordsListAdapter.kt
@@ -0,0 +1,40 @@
+package com.ancientlore.lexio
+
+import android.support.annotation.UiThread
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+
+class WordsListAdapter(private val items : MutableList<Word>) : RecyclerView.Adapter<WordsListAdapter.ViewHolder>() {
+
+	@UiThread
+	fun addItem(newWord: Word) {
+		items.add(newWord)
+		notifyItemInserted(itemCount - 1)
+	}
+
+	override fun getItemCount(): Int {
+		return items.count()
+	}
+
+	override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+		return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.dictionary_list_item, parent, false))
+	}
+
+	override fun onBindViewHolder(holder: ViewHolder, index: Int) {
+		holder.bind(items[index])
+	}
+
+	class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+		private val titleView: TextView = itemView.findViewById(R.id.title)
+		private val subtitleView: TextView = itemView.findViewById(R.id.subtitle)
+
+		fun bind(data: Word) {
+			titleView.text = data.name
+			subtitleView.text = data.translation
+		}
+	}
+}
